b72bef120ede9d7709c8b9e523cbaccec8eb1051
Shubham Kalihari <shubhamkalihari09@gmail.com>
2021-04-15 15:49:15 +0530

    added doxygen comments

--- 3_Implementation/inc/salary_management.h
+++ 3_Implementation/inc/salary_management.h
@@ -21,6 +21,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * @brief Structure to store the retrived information of employee
+ * 
+ */
 typedef struct employee
 {
     
@@ -31,30 +35,86 @@ typedef struct employee
 }emp;
 
 
-
+/**
+ * @brief Get the details all of the employee
+ * 
+ */
 void get_details_all();
 
-
+/**
+ * @brief Add new employee to the database
+ * 
+ */
 void add_employee();
 
-
+/**
+ * @brief Get the salary by id of the employee
+ * 
+ * @param id 
+ * @return long 
+ */
 long get_salary_by_id(char id[20]);
 
-
+/**
+ * @brief Delete the employee from the database
+ * 
+ * @param id 
+ */
 void del_employee(char id[20]);
 
+/**
+ * @brief Get the row which contain the given id of the employee
+ * 
+ * @param id 
+ * @return int 
+ */
 int get_row(char id[20]);
 
+/**
+ * @brief Delete the line(Data of employee) from the database
+ * 
+ * @param srcFile 
+ * @param tempFile 
+ * @param line 
+ */
 void deleteLine(FILE *srcFile, FILE *tempFile, const int line);
 
+/**
+ * @brief print the content of database
+ * 
+ * @param fptr 
+ */
 void printFile(FILE *fptr);
 
+/**
+ * @brief Increment the salary of an employee
+ * 
+ * @param id 
+ */
 void increment(char id[20]);
 
+/**
+ * @brief Get the details of employee by id
+ * 
+ * @param id 
+ * @return emp 
+ */
 emp get_details_employee(char id[20]);
 
+/**
+ * @brief Calculate the the payable salary of the employee for current month
+ * 
+ * @param id 
+ * @return float 
+ */
 float calculate_salary(char id[20]);
 
+/**
+ * @brief Convert long to string
+ * 
+ * @param str 
+ * @param num 
+ */
 void toString(char str[], long num);
 
 
