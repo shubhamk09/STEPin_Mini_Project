14c175ff70ab8ea833551f432f8a604eacf7466f
Shubham Kalihari <shubhamkalihari09@gmail.com>
2021-04-16 02:55:32 +0530

    tes

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -6,17 +6,20 @@
 	void tearDown()
 	{	}
 	
-	void test_sum(void)
+	void test_get_details_employee(void)
 	{
-  		// TEST_ASSERT_EQUAL(30, sum(10, 20));
-  		// TEST_ASSERT_EQUAL(-30, sum(-50, 20));
+		emp employee={};
+		emp employee_check={11710408, "shubham", "software", 420040};
+		employee=get_details_employee("11710408");
+		TEST_ASSERT_EQUAL(employee_check.id, employee.id);
+		
 	}
 
 	int test_main(void)
 	{
 	  	UNITY_BEGIN();
   	
-	  	RUN_TEST(test_sum);
+	  	RUN_TEST(test_get_details_employee);
 
   		return UNITY_END();
 	}
